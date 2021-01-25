143ece40b3b92ca55908644b034b1da4f8c783ea
Heidi <info@risako.fi>
2021-01-25 14:42:03 +0200

    30. Optional: Skip The Automated Workflow Entirely

--- app/public/wp-content/themes/fictional_university_thema/functions.php
+++ app/public/wp-content/themes/fictional_university_thema/functions.php
@@ -1,19 +1,22 @@
 <?php
 
 function university_files() {
-  wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-  wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   
-  if (strstr($_SERVER['SERVER_NAME'], 'functionaluniversity.local')) {
-    wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js' , NULL, '1.0', true);
-  }else {
-    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.bc49dbb23afb98cfc0f7.css'));
-  }
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=yourkeygoeshere', NULL, '1.0', true);
+  wp_enqueue_script('axios', '//cdn.jsdelivr.net/npm/axios/dist/axios.min.js', NULL, '1.0', true);
+  wp_enqueue_script('glidejs', '//cdn.jsdelivr.net/npm/@glidejs/glide', NULL, '1.0', true);
 
-  
+  wp_enqueue_script('main-university-js', get_theme_file_uri('/scripts.js'), array('jquery'), '1.0', true);
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+
+  wp_localize_script('main-university-js', 'universityData', array(
+    'root_url' => get_site_url(),
+    'nonce' => wp_create_nonce('wp_rest')
+  ));
 }
+
 
 add_action('wp_enqueue_scripts', 'university_files');
 
